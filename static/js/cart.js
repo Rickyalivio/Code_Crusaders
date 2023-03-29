b3540650fbb5d2bc4a0fa629cb1e0728c71faa55
unknown <james.acal@zdspgc.edu.ph>
2023-03-30 02:40:57 +0800

    hapit nako mabuang

--- static/js/cart.js
+++ static/js/cart.js
@@ -4,11 +4,11 @@ for (var i = 0; i < updateBtns.length; i++) {
     updateBtns[i].addEventListener('click', function(){
         var productId = this.dataset.product
         var action = this.dataset.action
-        console.log('productId', productId, 'action', action)
+        console.log('productId:', productId, 'action:', action)
 
         console.log('USER:', user)
-        if(user=== 'AnonymousUser'){
-            console.log('Not logged in')
+        if(user === 'AnonymousUser'){
+            console.Log('Not logged in')
         }else{
             updateUserOrder(productId, action)
         }
@@ -20,10 +20,11 @@ function updateUserOrder(productId, action){
     var url = '/update_item/'
 
     fetch(url, {
-        method:'POST',
+        method:"POST",
         headers:{
             'Content-Type':'application/json',
             'X-CSRFToken':csrftoken,
+
         },
         body:JSON.stringify({'productId': productId, 'action':action})
     })
@@ -34,5 +35,6 @@ function updateUserOrder(productId, action){
 
     .then((data) => {
         console.log('data:', data)
+        location.reload()
     })
 }
